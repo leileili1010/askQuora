5fa20afd8ce49a52aa7894a56fe850bf4b7297ab
Lei Li <lei.xw.li@hotmail.com>
2024-02-29 09:31:15 -0800

    to deploy

--- react-vite/src/redux/question.js
+++ react-vite/src/redux/question.js
@@ -1,18 +1,18 @@
-// action type
-const RETURN_INITIAL = "questions/RETURN_INITIAL";
-const GET_QUESTIONS = "questions/GET_QUESTIONS";
+// // action type
+// const RETURN_INITIAL = "questions/RETURN_INITIAL";
+// const GET_QUESTIONS = "questions/GET_QUESTIONS";
 
-// action creator
-const getProjects = (projects) => {
-    return {
-      type: GET_PROJECTS,
-      projects,
-    };
-  };
+// // action creator
+// const getProjects = (projects) => {
+//     return {
+//       type: GET_PROJECTS,
+//       projects,
+//     };
+//   };
   
 
-const getOneProject = (project) => ({
-    type: GET_ONE_PROJECT,
-    project,
-  });
+// const getOneProject = (project) => ({
+//     type: GET_ONE_PROJECT,
+//     project,
+//   });
   
