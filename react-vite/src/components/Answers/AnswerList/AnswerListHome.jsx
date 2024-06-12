00a9579d327e3b41e496c093e2c65cc0d50fc866
Lei Li <lei.xw.li@hotmail.com>
2024-06-12 14:22:58 -0700

    styling spinner

--- react-vite/src/components/Answers/AnswerList/AnswerListHome.jsx
+++ react-vite/src/components/Answers/AnswerList/AnswerListHome.jsx
@@ -1,6 +1,6 @@
 import AnswerListItem from "./AnswerListItem";
-import { useEffect, useState} from "react";
-import { useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetAllAnswers } from "../../../redux/answer";
 import InfiniteScroll from 'react-infinite-scroll-component';
@@ -15,26 +15,19 @@ const AnswerListHome = () => {
     const [editA, setEditA] = useState(0)
     const [page, setPage] = useState(1)
     const [hasMore, setHasMore] = useState(true);
-    
+
     const selectAnswers = state => state.answers;
-    
+
     const selectSortedAnswers = createSelector(
         [selectAnswers],
         (answers) => Object.values(answers).sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
-        );
-        const [isLoading, setIsLoading] = useState(false);
-        const sortedAnswers =useSelector(selectSortedAnswers);
-
-    // useEffect(() => {
-    //     setIsLoading(true);
-    //     setTimeout(() => {
-    //         setIsLoading(false);
-    //     }, 1500); 
-    // }, []);
+    );
+    // const [isLoading, setIsLoading] = useState(false);
+    const sortedAnswers = useSelector(selectSortedAnswers);
 
     useEffect(() => {
         const fetchData = async () => {
-            setIsLoading(true);
+            // setIsLoading(true);
             try {
                 const result = await dispatch(thunkGetAllAnswers(page));
                 if (result.length === 0) {
@@ -43,7 +36,7 @@ const AnswerListHome = () => {
             } catch (error) {
                 console.error('Error fetching data:', error);
             } finally {
-                setIsLoading(false);
+                // setIsLoading(false);
             }
         };
 
@@ -53,13 +46,7 @@ const AnswerListHome = () => {
 
     useEffect(() => {
         if (!user) navigate("/");
-      }, [user, navigate]);
-
-    //   if (isLoading) return(
-    //     <div className="answer-list-component">
-    //         <div className="spinner">Loading...</div>
-    //     </div>
-    //   )
+    }, [user, navigate]);
 
     if (!sortedAnswers.length) return null;
 
@@ -67,20 +54,23 @@ const AnswerListHome = () => {
         <div className="answer-list-component">
             <InfiniteScroll
                 dataLength={sortedAnswers.length}
-                next = {() => setPage(page + 1)}
+                next={() => setPage(page + 1)}
                 hasMore={hasMore}
                 loader={
-                    <div className="loading-spinner">
-                        <div className="loading-dot"></div>
-                        <div className="loading-dot"></div>
-                        <div className="loading-dot"></div>
+                    <div className="spinner-container">
+                        <div className="loading-spinner">
+                            <div className="loading-dot"></div>
+                            <div className="loading-dot"></div>
+                            <div className="loading-dot"></div>
+                        </div>
                     </div>
                 }
             >
                 {sortedAnswers.map(answer =>
-                <AnswerListItem answer={answer} setDeleteA={setDeleteA} setEditA={setEditA} key={answer.id} />    
+                    <AnswerListItem answer={answer} setDeleteA={setDeleteA} setEditA={setEditA} key={answer.id} />
                 )}
             </InfiniteScroll>
+
         </div>
     )
 }
