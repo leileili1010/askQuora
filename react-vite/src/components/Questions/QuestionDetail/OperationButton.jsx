25c5579b672f0ae04dfea84b845f7a76fa3cb174
Lei Li <lei.xw.li@hotmail.com>
2024-07-01 08:34:03 -0700

    fixed a display bug

--- react-vite/src/components/Questions/QuestionDetail/OperationButton.jsx
+++ react-vite/src/components/Questions/QuestionDetail/OperationButton.jsx
@@ -9,7 +9,7 @@ const OperationButton = ({question, setDeleteQ, setEditQ}) => {
     const ulRef = useRef();
 
     const toggleMenu = (e) => {
-        e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+        e.stopPropagation(); 
         setShowMenu(!showMenu);
       };
 
